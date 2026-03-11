547ad670312ea57954b77cff079fc82b17222744
Timexo <166215287+realTimexo@users.noreply.github.com>
2026-03-11 18:40:47 +0100

    Update zip-script.cjs

--- zip-script.cjs
+++ zip-script.cjs
@@ -1,25 +1,26 @@
 const fs = require('fs');
 const archiver = require('archiver');
 
-const manifest = JSON.parse(fs.readFileSync('./manifest.json', 'utf8'));
+const targetBrowser = process.argv[2] || 'chrome'; // chrome oder firefox
+
+// Manifest kopieren
+const manifestFile = targetBrowser === 'firefox' ? 'manifest.firefox.json' : 'manifest.chrome.json';
+fs.copyFileSync(manifestFile, './dist/manifest.json');
+
+const manifest = JSON.parse(fs.readFileSync('./dist/manifest.json', 'utf8'));
 const version = manifest.version;
-
 const cleanName = manifest.name.replace(/\s+/g, '-');
-
-const targetBrowser = process.argv[2] || 'Build';
 const browserLabel = targetBrowser.charAt(0).toUpperCase() + targetBrowser.slice(1);
-
 const fileName = `${cleanName}-${version}-${browserLabel}.zip`;
 
 const output = fs.createWriteStream(fileName);
 const archive = archiver('zip', { zlib: { level: 9 } });
 
 output.on('close', () => {
-console.log(`✅ ZIP erstellt: ${fileName} (${archive.pointer()} bytes)`);
+  console.log(`✅ ZIP erstellt: ${fileName} (${archive.pointer()} bytes)`);
 });
 
 archive.on('error', (err) => { throw err; });
-
 archive.pipe(output);
 archive.directory('dist/', false);
 archive.finalize();
